e5870f975b1166204d8405a5a51081432fd14330
Alanna Bailey <abaileyjane@gmail.com>
2018-04-11 19:45:16 -0400

    cleaned up package.json

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   // other stuff
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:8080"
+    "https://day-by-day-api.herokuapp.com/"
 };
